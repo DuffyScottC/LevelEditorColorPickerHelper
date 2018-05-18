7d0dff057688ca3effd43cb02d8089c719cb3828
Scott Duffy <duffyscottc@gmail.com>
2018-05-18 19:15:25 -0400

    Added elements to MainFrame

--- src/main/java/views/MainFrame.java
+++ src/main/java/views/MainFrame.java
@@ -26,19 +26,103 @@ public class MainFrame extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        selectionPanel = new javax.swing.JPanel();
+        typePanel = new javax.swing.JPanel();
+        typeLabel = new javax.swing.JLabel();
+        typeComboBox = new javax.swing.JComboBox<>();
+        searchPanel = new javax.swing.JPanel();
+        searchLabel = new javax.swing.JLabel();
+        searchTextField = new javax.swing.JTextField();
+        resultsPanel = new javax.swing.JPanel();
+        resultsScrollPane = new javax.swing.JScrollPane();
+        resultsList = new javax.swing.JList<>();
+        recentPanel = new javax.swing.JPanel();
+        recentScrollPane = new javax.swing.JScrollPane();
+        recentList = new javax.swing.JList<>();
+        infoPanel = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        selectionPanel.setLayout(new java.awt.GridBagLayout());
+
+        typeLabel.setText("Type:");
+        typePanel.add(typeLabel);
+
+        typeComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        typePanel.add(typeComboBox);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
+        selectionPanel.add(typePanel, gridBagConstraints);
+
+        searchLabel.setText("Search:");
+        searchPanel.add(searchLabel);
+
+        searchTextField.setText("Name");
+        searchPanel.add(searchTextField);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
+        selectionPanel.add(searchPanel, gridBagConstraints);
+
+        resultsPanel.setLayout(new java.awt.BorderLayout());
+
+        resultsList.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        resultsScrollPane.setViewportView(resultsList);
+
+        resultsPanel.add(resultsScrollPane, java.awt.BorderLayout.CENTER);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 0.1;
+        gridBagConstraints.weighty = 0.1;
+        selectionPanel.add(resultsPanel, gridBagConstraints);
+
+        recentPanel.setLayout(new java.awt.BorderLayout());
+
+        recentList.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        recentScrollPane.setViewportView(recentList);
+
+        recentPanel.add(recentScrollPane, java.awt.BorderLayout.CENTER);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 0.1;
+        gridBagConstraints.weighty = 0.1;
+        selectionPanel.add(recentPanel, gridBagConstraints);
+
+        getContentPane().add(selectionPanel, java.awt.BorderLayout.WEST);
+
+        javax.swing.GroupLayout infoPanelLayout = new javax.swing.GroupLayout(infoPanel);
+        infoPanel.setLayout(infoPanelLayout);
+        infoPanelLayout.setHorizontalGroup(
+            infoPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 400, Short.MAX_VALUE)
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        infoPanelLayout.setVerticalGroup(
+            infoPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 300, Short.MAX_VALUE)
         );
+
+        getContentPane().add(infoPanel, java.awt.BorderLayout.EAST);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -79,5 +163,19 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel infoPanel;
+    private javax.swing.JList<String> recentList;
+    private javax.swing.JPanel recentPanel;
+    private javax.swing.JScrollPane recentScrollPane;
+    private javax.swing.JList<String> resultsList;
+    private javax.swing.JPanel resultsPanel;
+    private javax.swing.JScrollPane resultsScrollPane;
+    private javax.swing.JLabel searchLabel;
+    private javax.swing.JPanel searchPanel;
+    private javax.swing.JTextField searchTextField;
+    private javax.swing.JPanel selectionPanel;
+    private javax.swing.JComboBox<String> typeComboBox;
+    private javax.swing.JLabel typeLabel;
+    private javax.swing.JPanel typePanel;
     // End of variables declaration//GEN-END:variables
 }
