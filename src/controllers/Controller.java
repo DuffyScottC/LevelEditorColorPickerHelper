636f9d9a70f9f9aad7fb54a9c636027b3f4d6462
Scott Duffy <duffyscottc@gmail.com>
2018-05-19 13:35:51 -0400

    changed from maven to normal and removed Gson code

---
+++ src/controllers/Controller.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import entities.Entity;
+import java.awt.Color;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import views.MainFrame;
+
+public class Controller {
+
+    private final MainFrame frame = new MainFrame();
+
+    public Controller() throws URISyntaxException {
+        frame.setTitle(getClass().getSimpleName());
+        frame.setLocationRelativeTo(null);
+        // you can adjust the size with something like this:
+        // frame.setSize(600, 500);
+        
+        
+        System.out.println(new File(Controller.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath());
+    }
+
+    public static void main(String[] args) {
+        Controller app;
+        try {
+            app = new Controller();
+            app.frame.setVisible(true);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
