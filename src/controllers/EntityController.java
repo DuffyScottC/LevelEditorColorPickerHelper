ace5ccf46efe857d5030bbb63285423e81d802a9
Scott Duffy <duffyscottc@gmail.com>
2018-05-20 18:51:23 -0400

    added entitycontroller

---
+++ src/controllers/EntityController.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+/**
+ * Handles the creation of new entities, the modification and deletion of
+ * existing entities, and other entity management processes.
+ * @author Scott
+ */
+public class EntityController {
+    
+}
