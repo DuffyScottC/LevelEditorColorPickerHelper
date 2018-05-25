f53adb0a7a7e60338cee72176644504463150639
Scott Duffy <duffyscottc@gmail.com>
2018-05-25 19:27:25 -0400

    tried to move code to infopanelcontroller

--- src/controllers/InfoPanelController.java
+++ src/controllers/InfoPanelController.java
@@ -5,7 +5,22 @@
  */
 package controllers;
 
+import entities.Entity;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.xml.bind.JAXBException;
+import projects.Project;
 import views.MainFrame;
+import views.NewProjectDialog;
 
 /**
  * Responsible for adding action listeners to, controlling access to, parsing
@@ -14,8 +29,173 @@ import views.MainFrame;
  */
 public class InfoPanelController {
     
+    private final MainFrame frame;
+    private ProjectController projectController = null;
+    private Project currentProject = null;
+    
+    private final NewProjectDialog newProjectDialog;
+    
     public InfoPanelController(MainFrame frame) {
         
+        this.frame = frame;
+        
+        //set up new project dialog
+        newProjectDialog = new NewProjectDialog(frame, true);
+        setUpNewProjectDialogActionListeners(frame);
+        
+        frame.getNewProjectMenuItem().addActionListener((ActionEvent e) -> {
+            openNewProjectDialog();
+        });
+        
+        //action listener for add entity Button and add entity MenuItem
+        ActionListener addEntityActionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //if the user just opened the app and no project has been selected yet
+                if (currentProject != null) {
+                    try {
+                        //generate a new default entity
+                        Entity newEntity = Project.generateNewDefaultEntity();
+                        //add the new entity to the currentProject
+                        currentProject.addEntity(newEntity);
+                        currentProject.setCurrentEntity(newEntity);
+                        //update the UI to reflect the creation of a new entity
+                        loadEntityIntoInfoPanel(newEntity);
+                        
+                        //tell the project that it has been modified
+                        setIsModified(true);
+                        setInfoElementsEnabled(true);
+                        //in this case, there is nothing to revert to
+                        frame.getRevertButton().setEnabled(false);
+                    } catch (Exception ex) {
+                        //the one with no explaination
+                        JOptionPane.showMessageDialog(frame,  "Technical "
+                                + "difficulties:\n"
+                                + ex.toString());
+                    }
+                } else {
+                    //open the new project dialog
+                    openNewProjectDialog();
+                }
+            }
+        };
+        
+        //add the action listener to both the button and the menu item
+        frame.getAddEntityButton().addActionListener(addEntityActionListener);
+        frame.getAddEntityMenuItem().addActionListener(addEntityActionListener);
+        
+    }
+    
+    public void setProjectController(ProjectController projectController) {
+        this.projectController = projectController;
+    }
+    
+    public void setCurrentProject(Project currentProject) {
+        this.currentProject = currentProject;
+    }
+    
+    //MARK: New Project
+    private void openNewProjectDialog() {
+        //reset the dialog
+        resetNewProjectDialog(frame);
+        newProjectDialog.setVisible(true);
+    }
+    
+    private void resetNewProjectDialog(MainFrame frame) {
+        newProjectDialog.setName("New Project");
+        newProjectDialog.setLocationRelativeTo(null);
+        
+        JButton finishButton = newProjectDialog.getFinishButton();
+        
+        //make it so that the user can press enter to finish
+        newProjectDialog.getRootPane().setDefaultButton(finishButton);
+        
+        JTextField pNameTextField 
+                = newProjectDialog.getProjectNameTextField();
+        JTextField pLocationTextField 
+                = newProjectDialog.getProjectLocationTextField();
+        JTextField pFolderTextField 
+                = newProjectDialog.getProjectFolderTextField();
+        pNameTextField.requestFocus();
+        pNameTextField.selectAll();
+        
+        //initialize the project location
+        projectLocation = new File(System.getProperty("user.dir"));
+        
+        pNameTextField.setText("New Project");
+        pLocationTextField.setText(System.getProperty("user.dir"));
+        Path defaultPath = Paths.get(System.getProperty("user.dir"), "New Project");
+        pFolderTextField.setText(defaultPath.toString());
+    }
+    
+    private void setUpNewProjectDialogActionListeners(MainFrame frame) {
+        resetNewProjectDialog(frame);
+        
+        JButton cancelButton = newProjectDialog.getCancelButton();
+        JButton finishButton = newProjectDialog.getFinishButton();
+        
+        //make it so that the user can press enter to finish
+        newProjectDialog.getRootPane().setDefaultButton(finishButton);
+        
+        JTextField pNameTextField 
+                = newProjectDialog.getProjectNameTextField();
+        JTextField pLocationTextField 
+                = newProjectDialog.getProjectLocationTextField();
+        
+        //If the user clicks the browse button
+        newProjectDialog.getBrowseButton().addActionListener((ActionEvent e) -> {
+            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            int result = chooser.showOpenDialog(frame);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File tempProjectLocation = chooser.getSelectedFile();
+                if (tempProjectLocation.exists()) {
+                    if (tempProjectLocation.isDirectory()) {
+                        //assign the project location.
+                        projectLocation = tempProjectLocation;
+                        //set the project location
+                        pLocationTextField.setText(projectLocation.getAbsolutePath());
+                        
+                        updateNewProjectFolderTextBox();
+                    }
+                }
+            }
+            chooser.resetChoosableFileFilters();
+        });
+        
+        pNameTextField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                projectName = pNameTextField.getText();
+                //every time the user presses a key, update the project folder
+                updateNewProjectFolderTextBox();
+            }
+        });
+        
+        cancelButton.addActionListener((ActionEvent e) -> {
+            newProjectDialog.setVisible(false);
+        });
+        
+        finishButton.addActionListener((ActionEvent e) -> {
+            try {
+                //if we successfully create a new project
+                if (createNewProject()) {
+                    newProjectDialog.setVisible(false);
+                }
+            } catch (JAXBException ex) {
+                //if the creation of the project was not successful,
+                //output the exception message to the user.
+                JOptionPane.showMessageDialog(frame, "Unable to serialize project.\n" 
+                        + ex.toString());
+                //allow the user to try again by staying still and keeping
+                //the newProjectDialog window open.
+            } catch (Exception ex) {
+                //if the creation of the project was not successful,
+                //output the exception message to the user.
+                JOptionPane.showMessageDialog(frame, ex.toString());
+                //allow the user to try again by staying still and keeping
+                //the newProjectDialog window open.
+            }
+        });
     }
     
 }
--- src/controllers/ProjectController.java
+++ src/controllers/ProjectController.java
@@ -57,6 +57,7 @@ public class ProjectController {
     private final ResultsListController resultsListController;
     private final RecentListController recentListController;
     private final ColorPickerController colorPickerController;
+    private final InfoPanelController infoPanelController;
     
     /**
      * initialized with user.dir just in case something goes wrong with loading
@@ -70,7 +71,6 @@ public class ProjectController {
      */
     private final FileFilter projectFileFilter 
             = new FileNameExtensionFilter("Project (.lecp)","lecp");
-    private final NewProjectDialog newProjectDialog;
     
     private boolean isModified = false;
     
@@ -92,8 +92,10 @@ public class ProjectController {
         this.resultsListController = resultsListController;
         this.recentListController = recentListController;
         this.colorPickerController = colorPickerController;
+        this.infoPanelController = infoPanelController;
         
-        newProjectMenuItem = frame.getNewProjectMenuItem();
+        infoPanelController.setProjectController(this);
+        
         openProjectMenuItem = frame.getOpenProjectMenuItem();
         newEntityMenuItem = frame.getAddEntityMenuItem();
         deleteEntityMenuItem = frame.getDeleteEntityMenuItem();
@@ -101,54 +103,11 @@ public class ProjectController {
         //set up chooser
         chooser.setMultiSelectionEnabled(false);
         
-        //set up new project dialog
-        newProjectDialog = new NewProjectDialog(frame, true);
-        setUpNewProjectDialogActionListeners(frame);
         
-        newProjectMenuItem.addActionListener((ActionEvent e) -> {
-            openNewProjectDialog();
-        });
         
         openProjectMenuItem.addActionListener((ActionEvent e) -> {
             openProject();
         });
-        
-        //action listener for add entity Button and add entity MenuItem
-        ActionListener addEntityActionListener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //if the user just opened the app and no project has been selected yet
-                if (currentProject != null) {
-                    try {
-                        //generate a new default entity
-                        Entity newEntity = generateNewDefaultEntity();
-                        //add the new entity to the currentProject
-                        currentProject.addEntity(newEntity);
-                        currentProject.setCurrentEntity(newEntity);
-                        //update the UI to reflect the creation of a new entity
-                        loadEntityIntoInfoPanel(newEntity);
-                        
-                        //tell the project that it has been modified
-                        setIsModified(true);
-                        setInfoElementsEnabled(true);
-                        //in this case, there is nothing to revert to
-                        frame.getRevertButton().setEnabled(false);
-                    } catch (Exception ex) {
-                        //the one with no explaination
-                        JOptionPane.showMessageDialog(frame,  "Technical "
-                                + "difficulties:\n"
-                                + ex.toString());
-                    }
-                } else {
-                    //open the new project dialog
-                    openNewProjectDialog();
-                }
-            }
-        };
-        
-        //add the action listener to both the button and the menu item
-        frame.getAddEntityButton().addActionListener(addEntityActionListener);
-        frame.getAddEntityMenuItem().addActionListener(addEntityActionListener);
         
         frame.getApplyButton().addActionListener((ActionEvent e) -> {
             loadEntityFromInfoPanelIntoProject();
@@ -322,7 +281,7 @@ public class ProjectController {
      */
     private void loadProject(Project newProject) {
         newProject.setUpTransientLists();
-        currentProject = newProject;
+        setCurrentProject(newProject);
         projectLocation = newProject.getProjectLocation();
         projectFile = newProject.getProjectFile();
         projectName = newProject.getName();
@@ -341,13 +300,6 @@ public class ProjectController {
          }
          return null;
      }
-    
-    //MARK: New Project
-    private void openNewProjectDialog() {
-        //reset the dialog
-        resetNewProjectDialog(frame);
-        newProjectDialog.setVisible(true);
-    }
     
     /**
      * Creates a new project in the current projectLocation by creating a new
@@ -419,7 +371,8 @@ public class ProjectController {
         //At this point, the name, locaiton, and file are assigned.
         //create the new project using the name, location, and file created
         //specifically for this project
-        currentProject = new Project(projectName, projectLocation, projectFile);
+        Project newCurrentProject = new Project(projectName, projectLocation, projectFile);
+        setCurrentProject(newCurrentProject);
         currentProject.addType(Utils.defaultType);
         //serialize the new project
         serializeNewProjectToXML();
@@ -478,102 +431,6 @@ public class ProjectController {
         marshaller.marshal(currentProject, projectFile);
     }
     
-    private void resetNewProjectDialog(MainFrame frame) {
-        newProjectDialog.setName("New Project");
-        newProjectDialog.setLocationRelativeTo(null);
-        
-        JButton finishButton = newProjectDialog.getFinishButton();
-        
-        //make it so that the user can press enter to finish
-        newProjectDialog.getRootPane().setDefaultButton(finishButton);
-        
-        JTextField pNameTextField 
-                = newProjectDialog.getProjectNameTextField();
-        JTextField pLocationTextField 
-                = newProjectDialog.getProjectLocationTextField();
-        JTextField pFolderTextField 
-                = newProjectDialog.getProjectFolderTextField();
-        pNameTextField.requestFocus();
-        pNameTextField.selectAll();
-        
-        //initialize the project location
-        projectLocation = new File(System.getProperty("user.dir"));
-        
-        pNameTextField.setText("New Project");
-        pLocationTextField.setText(System.getProperty("user.dir"));
-        Path defaultPath = Paths.get(System.getProperty("user.dir"), "New Project");
-        pFolderTextField.setText(defaultPath.toString());
-    }
-    
-    private void setUpNewProjectDialogActionListeners(MainFrame frame) {
-        resetNewProjectDialog(frame);
-        
-        JButton cancelButton = newProjectDialog.getCancelButton();
-        JButton finishButton = newProjectDialog.getFinishButton();
-        
-        //make it so that the user can press enter to finish
-        newProjectDialog.getRootPane().setDefaultButton(finishButton);
-        
-        JTextField pNameTextField 
-                = newProjectDialog.getProjectNameTextField();
-        JTextField pLocationTextField 
-                = newProjectDialog.getProjectLocationTextField();
-        
-        //If the user clicks the browse button
-        newProjectDialog.getBrowseButton().addActionListener((ActionEvent e) -> {
-            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            int result = chooser.showOpenDialog(frame);
-            if (result == JFileChooser.APPROVE_OPTION) {
-                File tempProjectLocation = chooser.getSelectedFile();
-                if (tempProjectLocation.exists()) {
-                    if (tempProjectLocation.isDirectory()) {
-                        //assign the project location.
-                        projectLocation = tempProjectLocation;
-                        //set the project location
-                        pLocationTextField.setText(projectLocation.getAbsolutePath());
-                        
-                        updateNewProjectFolderTextBox();
-                    }
-                }
-            }
-            chooser.resetChoosableFileFilters();
-        });
-        
-        pNameTextField.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyReleased(KeyEvent e) {
-                projectName = pNameTextField.getText();
-                //every time the user presses a key, update the project folder
-                updateNewProjectFolderTextBox();
-            }
-        });
-        
-        cancelButton.addActionListener((ActionEvent e) -> {
-            newProjectDialog.setVisible(false);
-        });
-        
-        finishButton.addActionListener((ActionEvent e) -> {
-            try {
-                //if we successfully create a new project
-                if (createNewProject()) {
-                    newProjectDialog.setVisible(false);
-                }
-            } catch (JAXBException ex) {
-                //if the creation of the project was not successful,
-                //output the exception message to the user.
-                JOptionPane.showMessageDialog(frame, "Unable to serialize project.\n" 
-                        + ex.toString());
-                //allow the user to try again by staying still and keeping
-                //the newProjectDialog window open.
-            } catch (Exception ex) {
-                //if the creation of the project was not successful,
-                //output the exception message to the user.
-                JOptionPane.showMessageDialog(frame, ex.toString());
-                //allow the user to try again by staying still and keeping
-                //the newProjectDialog window open.
-            }
-        });
-    }
     
     public void updateNewProjectFolderTextBox() {
         JTextField pFolderTextField = newProjectDialog.getProjectFolderTextField();
@@ -731,5 +588,10 @@ public class ProjectController {
         this.isModified = isModified;
         frame.getRevertButton().setEnabled(isModified);
         frame.getApplyButton().setEnabled(isModified);
+    }
+    
+    private void setCurrentProject(Project currentProject) {
+        this.currentProject = currentProject;
+        infoPanelController.setCurrentProject(currentProject);
     }
 }
