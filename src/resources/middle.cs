9ecbaf7d2175fff36d4b5f0bd431a2b6a4fbecf4
Scott Duffy <duffyscottc@gmail.com>
2018-07-06 15:41:15 -0400

    changed C# scripts to use offset when instantiating entities

--- src/resources/middle.cs
+++ src/resources/middle.cs
@@ -27,7 +27,11 @@
     /// <param name="y">The y coordinate of the pixel's position.</param>
     private void instantiateIfMatch(Entity entity, Color pixelColor, int x, int y) {
     if (entity.color.Equals(pixelColor)) {
+            //convert the grid position into actuall world coordinates
             Vector2 position = new Vector2(x * gridSize, y * gridSize);
+            //add the entity's offset to the position
+            position += entity.offset;
+            //instantiate the prefab
             Instantiate(entity.prefab, position, Quaternion.identity, transform);
         }
     }
@@ -43,5 +47,8 @@
         if (pixelColor.a == 0) {
             //ignore transparent pixels
             return;
+        }
+        if(pixelColor.r > 0) {
+            Debug.Log(pixelColor);
         }
         //end of middle section
