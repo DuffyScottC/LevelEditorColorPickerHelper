7f715ba8692358d9993fcd06081f7f6e203bd64f
Scott Duffy <duffyscottc@gmail.com>
2018-07-09 16:26:19 -0400

    changed scripts to include name as an entity attribute

--- src/resources/Entity.cs
+++ src/resources/Entity.cs
@@ -3,19 +3,26 @@ using UnityEngine;
 [System.Serializable]
 public class Entity
 {
-    
-	public Color color;
+	[HideInInspector]
+	public string name;
 	public GameObject prefab;
+    public Color color;
 	public Vector2 offset;
 
-	public Entity(Color color) {
+	public Entity(string name, Color color) {
+		this.name = name;
 		this.color = color;
 		this.offset = Vector2.zero;
 	}
 
-	public Entity(Color color, Vector2 offset) {
+	public Entity(string name, Color color, Vector2 offset) {
+		this.name = name;
         this.color = color;
         this.offset = offset;
     }
+
+	public string ToString() {
+		return name;
+	}
 
 }
