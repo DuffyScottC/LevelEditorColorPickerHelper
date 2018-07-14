3100ae01307f67be12277b07a5e43f267bc5b6cd
Scott Duffy <duffyscottc@gmail.com>
2018-07-13 20:39:29 -0400

    removed useless combobox

--- src/views/NewProjectDialog.java
+++ src/views/NewProjectDialog.java
@@ -41,8 +41,6 @@ public class NewProjectDialog extends javax.swing.JDialog {
         browseButton = new javax.swing.JButton();
         finishButton = new javax.swing.JButton();
         cancelButton = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
-        projectTypeComboBox = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -64,10 +62,6 @@ public class NewProjectDialog extends javax.swing.JDialog {
 
         cancelButton.setText("Cancel");
 
-        jLabel4.setText("Project Type:");
-
-        projectTypeComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -84,14 +78,12 @@ public class NewProjectDialog extends javax.swing.JDialog {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2)
                             .addComponent(jLabel1)
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel4))
+                            .addComponent(jLabel3))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(projectNameTextField, javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(projectFolderTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 1, Short.MAX_VALUE)
-                            .addComponent(projectLocationTextField, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 522, Short.MAX_VALUE)
-                            .addComponent(projectTypeComboBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(projectLocationTextField, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 522, Short.MAX_VALUE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(browseButton)))
                 .addContainerGap())
@@ -112,10 +104,6 @@ public class NewProjectDialog extends javax.swing.JDialog {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(projectFolderTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(projectTypeComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(finishButton)
@@ -175,11 +163,9 @@ public class NewProjectDialog extends javax.swing.JDialog {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JTextField projectFolderTextField;
     private javax.swing.JTextField projectLocationTextField;
     private javax.swing.JTextField projectNameTextField;
-    private javax.swing.JComboBox<String> projectTypeComboBox;
     // End of variables declaration//GEN-END:variables
 
     public JButton getBrowseButton() {
@@ -204,10 +190,6 @@ public class NewProjectDialog extends javax.swing.JDialog {
 
     public JButton getFinishButton() {
         return finishButton;
-    }
-
-    public JComboBox<String> getProjectTypeComboBox() {
-        return projectTypeComboBox;
     }
     
 }
